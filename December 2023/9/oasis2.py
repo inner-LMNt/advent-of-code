f4699644c84d3594391f960de79a2f4168e712c1
Kevin Huang <kevinhuang2011@gmail.com>
2023-12-09 05:22:30 +0000

    Dec 9 2023 Part 2

---
+++ December 2023/9/oasis2.py
@@ -0,0 +1,29 @@
+def solution1(arr):
+    temparr = arr.split(" ")
+    temparr.reverse() # This is the only change lol
+    n = len(temparr)
+    result = int(temparr[n-1])
+
+    while temparr.count("0") != len(temparr):
+        for i in range(n-1):
+            temparr[i] = str(int(temparr[i+1]) - int(temparr[i]))
+        result += int(temparr[i])
+        n -= 1
+        temparr.pop()
+
+    return result
+    
+
+if __name__ == '__main__':
+    file = open("oasis.txt", "r")
+    lines = []
+
+    for line in file:
+        lines.append(line.strip())
+
+    result = 0
+    for line in lines:
+        result += solution1(line)
+        
+    print(result)
+    
