aa8d70621a89bd7c5468bc2f621f3b57a2db4cc9
Kevin Huang <kevinhuang2011@gmail.com>
2023-12-16 21:01:08 +0000

    Dec 16 2023

---
+++ December 2023/16/lava1.py
@@ -0,0 +1,127 @@
+def solution1(input):
+    tracker = [[0 for _ in range(len(input[0]))] for _ in range(len(input))]
+    stack = [(0, 0, 'right')]
+
+    slash_set = set()
+
+    while stack:
+        row, col, direction = stack.pop()
+
+        if direction == 'right':
+            while col < len(input[0]):
+                if input[row][col] == '.' or input[row][col] == '-':
+                    tracker[row][col] = 1
+                    col += 1
+                elif input[row][col] == '|':
+                    if tracker[row][col] == 1:
+                        break
+                    tracker[row][col] = 1
+                    stack.append((row-1, col, 'up'))
+                    stack.append((row+1, col, 'down'))
+                    break
+                elif input[row][col] == '/':
+                    if (row, col, 'right', '/') in slash_set:
+                        break
+                    slash_set.add((row, col, 'right', '/'))
+                    tracker[row][col] = 1
+                    stack.append((row-1, col, 'up'))
+                    break
+                elif input[row][col] == '\\':
+                    if (row, col, 'right', '\\') in slash_set:
+                        break
+                    slash_set.add((row, col, 'right', '\\'))
+                    tracker[row][col] = 1
+                    stack.append((row+1, col, 'down'))
+                    break
+
+        elif direction == 'up':
+            while row >= 0:
+                if input[row][col] == '.' or input[row][col] == '|':
+                    tracker[row][col] = 1
+                    row -= 1
+                elif input[row][col] == '-':
+                    if tracker[row][col] == 1:
+                        break
+                    tracker[row][col] = 1
+                    stack.append((row, col+1, 'right'))
+                    stack.append((row, col-1, 'left'))
+                    break
+                elif input[row][col] == '/':
+                    if (row, col, 'up', '/') in slash_set:
+                        break
+                    slash_set.add((row, col, 'up', '/'))
+                    tracker[row][col] = 1
+                    stack.append((row, col+1, 'right'))
+                    break
+                elif input[row][col] == '\\':
+                    if (row, col, 'up', '\\') in slash_set:
+                        break
+                    slash_set.add((row, col, 'up', '\\'))
+                    tracker[row][col] = 1
+                    stack.append((row, col-1, 'left'))
+                    break
+
+        elif direction == 'left':
+            while col >= 0:
+                if input[row][col] == '.' or input[row][col] == '-':
+                    tracker[row][col] = 1
+                    col -= 1
+                elif input[row][col] == '|':
+                    if tracker[row][col] == 1:
+                        break
+                    tracker[row][col] = 1
+                    stack.append((row-1, col, 'up'))
+                    stack.append((row+1, col, 'down'))
+                    break
+                elif input[row][col] == '/':
+                    if (row, col, 'left', '/') in slash_set:
+                        break
+                    slash_set.add((row, col, 'left', '/'))
+                    tracker[row][col] = 1
+                    stack.append((row+1, col, 'down'))
+                    break
+                elif input[row][col] == '\\':
+                    if (row, col, 'left', '\\') in slash_set:
+                        break
+                    slash_set.add((row, col, 'left', '\\'))
+                    tracker[row][col] = 1
+                    stack.append((row-1, col, 'up'))
+                    break
+
+        elif direction == 'down':
+            while row < len(input):
+                if input[row][col] == '.' or input[row][col] == '|':
+                    tracker[row][col] = 1
+                    row += 1
+                elif input[row][col] == '-':
+                    if tracker[row][col] == 1:
+                        break
+                    tracker[row][col] = 1
+                    stack.append((row, col+1, 'right'))
+                    stack.append((row, col-1, 'left'))
+                    break
+                elif input[row][col] == '/':
+                    if (row, col, 'down', '/') in slash_set:
+                        break
+                    slash_set.add((row, col, 'down', '/'))
+                    tracker[row][col] = 1
+                    stack.append((row, col-1, 'left'))
+                    break
+                elif input[row][col] == '\\':
+                    if (row, col, 'down', '\\') in slash_set:
+                        break
+                    slash_set.add((row, col, 'down', '\\'))
+                    tracker[row][col] = 1
+                    stack.append((row, col+1, 'right'))
+                    break
+
+    print(sum([sum(row) for row in tracker]))
+
+if __name__ == '__main__':
+    file_path = 'lava.txt'
+    data = []
+    with open(file_path, 'r') as f:
+        for line in f:
+            data.append(list(line.strip()))
+
+    solution1(data)
