afc65133c117f211dafaaa6732bee054d85cb74e
Kevin Huang <kevinhuang2011@gmail.com>
2023-12-24 08:08:34 +0000

    Dec 12 2023 Part 2

--- December 2023/12/onsen2.py
+++ December 2023/12/onsen2.py
@@ -1,24 +1,25 @@
 def count_ways(dots, blocks, dot_index, block_index, current, dp):    
-    if (dot_index, block_index, current) in dp:
-        return dp[(dot_index, block_index, current)]
+    key = (dot_index, block_index, current)
+    if key in dp:
+        return dp[key]
     
     if dot_index == len(dots):
         if (block_index == len(blocks) and current == 0) or (block_index == len(blocks)-1 and blocks[block_index] == current):
             return 1
-        else:
-            return 0
+        return 0
     
     count = 0
-    for c in ['#', '.']:
-        if dots[dot_index] == c or dots[dot_index] == '?':
-            if c == '#':
-                count += count_ways(dots, blocks, dot_index+1, block_index, current+1, dp)
-            elif c == '.' and current == 0:
-                count += count_ways(dots, blocks, dot_index+1, block_index, 0, dp)
-            elif block_index < len(blocks) and blocks[block_index] == current:
-                count += count_ways(dots, blocks, dot_index+1, block_index+1, 0, dp)
+        
+    if dots[dot_index] == '#' or dots[dot_index] == '?':
+        count += count_ways(dots, blocks, dot_index+1, block_index, current+1, dp)
+
+    if dots[dot_index] == '.' or dots[dot_index] == '?':
+        if current == 0:
+            count += count_ways(dots, blocks, dot_index+1, block_index, 0, dp)
+        elif block_index < len(blocks) and blocks[block_index] == current:
+            count += count_ways(dots, blocks, dot_index+1, block_index+1, 0, dp)
     
-    dp[(dot_index, block_index, current)] = count
+    dp[key] = count
     return count
 
 def solution2(input):
